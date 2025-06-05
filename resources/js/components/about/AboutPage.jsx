ca107cadf97e1a4c745c5598ae0c342d9a33a2f7
RomitWorkSpace <romitkumar848@gmail.com>
2025-06-05 09:46:03 +0530

    About us page completed

--- resources/js/components/about/AboutPage.jsx
+++ resources/js/components/about/AboutPage.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import PageBanner from '../common/PageBanner'
 import DkBanner from 'imgPath/banners/dk-about-banner.jpg'
+import MbBanner from 'imgPath/banners/mb-about-banner.png'
+import AboutCompany from './components/AboutCompany'
+import AboutOwner from './components/AboutOwner'
+import SimpleBreadcrumb from '../common/SimpleBreadcrumb'
+import TeamProfile from './components/TeamProfile'
+import OnPage from '../common/OnPage'
 
 function AboutPage() {
     return (
         <>
-        <PageBanner Banner={DkBanner} MobileBanner={DkBanner} PageName="About us" />
+        <OnPage MetaTitle="About Realty Research – Trusted Real Estate Advisors in Noida"
+        Keywords="about Realty Research, real estate consultants Noida, Noida property advisors, property experts Noida"
+        MetaDesc="Learn about Realty Research, Noida’s most trusted property advisory service, helping you find the perfect home or commercial space with expert guidance."
+        />
+        <PageBanner Banner={DkBanner} MobileBanner={MbBanner} PageName="ABOUT US" />
+        <SimpleBreadcrumb CurrentPage="About Us" />
+        <AboutCompany />
+        <TeamProfile />
         </>
     )
 }
