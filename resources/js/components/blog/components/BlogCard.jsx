06fa977ea432153ccbcb196594f9098762e98991
RomitWorkSpace <romitkumar848@gmail.com>
2025-06-23 17:42:23 +0530

    Blog dynamic content used

--- resources/js/components/blog/components/BlogCard.jsx
+++ resources/js/components/blog/components/BlogCard.jsx
@@ -1,22 +1,27 @@
 import React from 'react'
 import Blog from 'imgPath/blog1.jpg'
 import { FaRegCalendarAlt } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 
-function BlogCard() {
+function BlogCard({Title, Thumbnail, Slug, Description, PublishDate}) {
     return (
         <>
         <div className="blog-card shadow-lg mb-6">
-            <img src={Blog} alt="blog" width="100%" />
+            <img src={`/images/blogs/${Thumbnail}`} alt="blog" width="100%" />
             <div className="p-6">
-                <div className="inline-block primary-bg text-white px-5 py-2">Business</div>
-                <h2 className="text-gray-800 font-semibold text-xl md:text-3xl mt-5">Lorem Ipsum is simply dummy text of the printing and typesetting industry</h2>
+                <div className="inline-block primary-bg text-white px-5 py-2">Real Estate</div>
+                <h2 className="text-gray-800 font-semibold text-xl md:text-3xl mt-5">{Title}</h2>
 
-                <div className="py-5">
-                    <p className="text-gray-600 text-justify">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <div className="py-5 text-gray-600 text-justify">
+                <div
+                    dangerouslySetInnerHTML={{__html: Description.substr(0, 250)+"..."}}
+                />
                 </div>
                 <div className="flex py-3 justify-between">
-                    <div className="flex items-center gap-2"><FaRegCalendarAlt className='secondary-color' /> <span className="text-gray-500">July 26,2023</span></div>
+                    <div className="flex items-center gap-2"><FaRegCalendarAlt className='secondary-color' /> <span className="text-gray-500">{PublishDate}</span></div>
+                    <Link to={`/blog/${Slug}`}>
                     <div className="secondary-color">READ MORE</div>
+                    </Link>
                 </div>
             </div>
         </div>
