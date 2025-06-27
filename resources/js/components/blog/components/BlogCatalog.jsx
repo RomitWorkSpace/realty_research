d86a05b8abda6963f491fa415c2bf75b1c1a617d
RomitWorkSpace <romitkumar848@gmail.com>
2025-06-27 20:03:42 +0530

    Api used to get data

--- resources/js/components/blog/components/BlogCatalog.jsx
+++ resources/js/components/blog/components/BlogCatalog.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import BlogCard from './BlogCard'
+import axios from 'axios';
 
 function BlogCatalog() {
+
+    const [blogs, setBlogs] = useState([]);
+    
+        useEffect(()=>{
+            axios.get('/api/get-blogs').then(res => {
+                setBlogs(res.data.get_blogs);
+            })
+        },[]);
+
     return (
         <>
         <div className="blog-bg px-4">
-            <BlogCard />
-            <BlogCard />
+            {blogs && blogs.map((item)=>(
+                <BlogCard Title={item.title} Thumbnail={item.image} Slug={item.slug} Description={item.description} PublishDate={item.created_at} />
+            ))}
         </div>
         </>
     )
