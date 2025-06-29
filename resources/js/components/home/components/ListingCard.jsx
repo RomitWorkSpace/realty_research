4203ade50756c99bc3bef4d7455742533fb81e6f
RomitWorkSpace <romitkumar848@gmail.com>
2025-06-29 18:48:52 +0530

    icons added to card and able to use dynamic data

--- resources/js/components/home/components/ListingCard.jsx
+++ resources/js/components/home/components/ListingCard.jsx
@@ -1,25 +1,37 @@
 import React from 'react'
 import { BiBed, BiBath } from "react-icons/bi";
+import { MdOutlineBedroomParent, MdOutlineViewArray } from 'react-icons/md';
+import { Link } from 'react-router-dom';
 
-function ListingCard({ Villa }) {
+function ListingCard({ PropertyImage, Price, Title, Description, Slug, PLocation, Pid, Bathroom, Area, Room }) {
     return (
         <>
+        <Link to={`/residential/${PLocation}/${Slug}/${Pid}`}>
         <div className="w-full shadow-md">
-            <img src={ Villa } alt="Property" width="100%" />
+            <img src={`/images/property/${PropertyImage}`} alt="Property" width="100%" />
             <div className="p-4">
-                <p className="text-orange-400 font-semibold">$34,999/month</p>
-                <h4 className="font-semibold mt-4 text-lg text-gray-800">New Apartment Nice View</h4>
-                <p className="text-gray-600 mt-4 text-sm">Beautiful Huge 1 Family House in the Heart of Westburry, Newly</p>
+                <p className="text-orange-400 font-semibold">₹ {Price > 99 ? Price/100 + ' Cr' : Price + ' Lacs'}</p>
+                <h4 className="font-semibold mt-4 text-lg text-gray-800">{Title}</h4>
+                <div className='text-gray-600 mt-4 text-sm text-justify'
+                    dangerouslySetInnerHTML={{__html: Description.substr(0, 80)+"..."}}
+                />
 
                 <div className="facility py-4">
                     <div className="flex">
-                        <div className="w-1/3 flex border-r-2">
-                            <span>2</span><span><BiBath /></span>
+                        <div className="w-1/4 flex border-r-2 items-center">
+                            <span className=''><BiBath /></span><span className='text-sm'>{Bathroom}</span>
+                        </div>
+                        <div className="w-2/4 flex border-r-2 items-center">
+                            <span className=''><MdOutlineViewArray /></span><span className='text-sm'>{Area} sqft</span>
+                        </div>
+                        <div className="w-1/4 flex items-center">
+                            <span className=''><MdOutlineBedroomParent /></span><span className='text-sm'>{Room}</span>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+        </Link>
         </>
     )
 }
