3388e13b8385bcbda3b0f703e84c6fad98136327
RomitWorkSpace <romitkumar848@gmail.com>
2025-07-07 22:38:23 +0530

    Project Listing page for residential or commercial property

--- resources/js/components/project/ProjectListing.jsx
+++ resources/js/components/project/ProjectListing.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import PageBanner from '../common/PageBanner'
 import DkBanner from 'imgPath/banners/dk-product-banner.png'
+import MbBanner from 'imgPath/banners/mb-product-banner.png'
 import ProjectFilter from './components/ProjectFilter'
+import { useParams } from 'react-router-dom'
+import CommercialFilter from './components/CommercialFilter'
 
 function ProjectListing() {
+
+    const {property_type} = useParams();
+
     return (
         <>
-        <PageBanner Banner={DkBanner} MobileBanner={DkBanner} PageName="Project Listing" />
-        <ProjectFilter />
+        <PageBanner Banner={DkBanner} MobileBanner={MbBanner} PageName="PROJECT LISTING" />
+        {property_type == "r" ? <ProjectFilter /> : <CommercialFilter /> }
         </>
     )
 }
