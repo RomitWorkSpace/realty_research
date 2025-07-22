dbf31f0893c9423081bbd76d1b846c4f3c5c7199
RomitWorkSpace <romitkumar848@gmail.com>
2025-07-22 22:36:15 +0530

    Swiper slider added for upcomming project

--- resources/js/components/home/components/UpcommingProject.jsx
+++ resources/js/components/home/components/UpcommingProject.jsx
@@ -1,46 +1,109 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Autoplay } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/autoplay';
 
-import Divyansh from 'imgPath/divyansh.png'
-import Ska from 'imgPath/ska.png'
-import Vilaasa from 'imgPath/vilaasa.png'
-import Residency from 'imgPath/residency.png'
-import Banner from 'imgPath/Banner.png'
+import AceEstate from 'imgPath/projects/ace-estate.png'
+import Amstoria from 'imgPath/projects/amstoria-bptp.png'
+import AtsProvince from 'imgPath/projects/ats-province.png'
+import AurumAlumini from 'imgPath/projects/aurum-alumini.png'
+import GodrejLogo from 'imgPath/projects/godrej-logo.png'
+import HeroHome from 'imgPath/projects/hero-home.png'
+import Logo from 'imgPath/projects/logo.png'
+import Nbcc from 'imgPath/projects/nbcc.png'
+import Orris from 'imgPath/projects/orris.png'
+import Ska from 'imgPath/projects/ska.png'
+import Svp from 'imgPath/projects/svp.png'
+import Popup from './Popup';
+import PopupController from './PopupController';
 
 
 function UpcommingProject() {
+
+    const [isPopupOpen, setIsPopupOpen] = useState(false);
+    const [popupContent, setPopupContent] = useState("");
+
+    const popupData = ()=>{
+      setPopupContent(Content);
+      setIsPopupOpen(true);
+    }
+
     return (
         <>
-        <div className="container-fluid pt-10 pb-10 mb-10 secondary-bg">
+        
+        <div className="container-fluid pt-10 pb-10 secondary-bg">
             <div className="max-w-3xl px-5 mx-auto">
                 <h2 className="section-heading primary-color text-center text-3xl font-semibold">New Projects</h2>
-                <div className="flex flex-wrap mt-8">
-                    <div className="md:w-1/2 w-full mb-5">
-                        <div className="flex">
-                            <div className="w-1/2">
-                                <img src={ Divyansh } alt="divyansh" width="80%" className='mx-auto' />
-                            </div>
-                            <div className="w-1/2">
+
+                <div className=" slider-bg mt-8">
+                    <Swiper
+                        slidesPerView={1}
+                        breakpoints={{
+                            360: {
+        
+                              slidesPerView: 2,
+                            },
+                            768: {
+            
+                              slidesPerView: 3,
+                            },
+                            1280: {
+            
+                                slidesPerView: 4,
+                              },
+                          }} 
+                        loop={true}
+                        spaceBetween={0}
+                        autoplay={{delay: 4000,
+                        pauseOnMouseEnter: true,
+                        disableOnInteraction: false}}
+                        pagination={{ clickable: true }}
+                        modules={[Autoplay]}
+                        onSlideChange={() => console.log('slide change')}
+                        onSwiper={(swiper) => console.log(swiper)} className="banner-section">
+        
+                            <SwiperSlide>
+                                <img src={ AceEstate } alt="Ace Estate" width="80%" className='mx-auto' />
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <img src={ Amstoria } alt="Amstoria" width="80%" className='mx-auto' />
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <img src={ AtsProvince } alt="Ats Province" width="80%" className='mx-auto' />
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <img src={ AurumAlumini } alt="Aurum Alumini" width="80%" className='mx-auto' />
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <img src={ GodrejLogo } alt="Godrej Logo" width="80%" className='mx-auto' />
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <img src={ HeroHome } alt="Hero Home" width="80%" className='mx-auto' />
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <img src={ Logo } alt="Logo" width="80%" className='mx-auto' />
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <img src={ Nbcc } alt="Nbcc" width="80%" className='mx-auto' />
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <img src={ Orris } alt="Orris" width="80%" className='mx-auto' />
+                            </SwiperSlide>
+                            <SwiperSlide>
                                 <img src={ Ska } alt="Ska" width="80%" className='mx-auto' />
-                            </div>
-                        </div>
-                    </div>
-                    <div className="md:w-1/2 w-full">
-                        <div className="flex">
-                            <div className="w-1/2">
-                                <img src={ Vilaasa } alt="Vilaasa" width="80%" className='mx-auto' />
-                            </div>
-                            <div className="w-1/2">
-                                <img src={ Residency } alt="Residency" width="80%" className='mx-auto' />
-                            </div>
-                        </div>
-                    </div>
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <img src={ Svp } alt="SVP" width="80%" className='mx-auto' />
+                            </SwiperSlide>
+                    </Swiper>
                 </div>
             </div>
         </div>
 
-        <div className="container-fluid">
-            <img src={ Banner } alt="Banner" width="100%" />
-        </div>
+
+        
         </>
     )
 }
