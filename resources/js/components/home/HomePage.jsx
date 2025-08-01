57aaaced71e83cd0fdc01df589a1efb4e5e3d58a
RomitWorkSpace <romitkumar848@gmail.com>
2025-08-01 22:45:22 +0530

    meta tage, banner slider and video testimonial component added

--- resources/js/components/home/HomePage.jsx
+++ resources/js/components/home/HomePage.jsx
@@ -7,17 +7,26 @@ import ImageSlider from './components/ImageSlider'
 import ProjectGallery from './components/ProjectGallery'
 import TestimonialSection from './components/TestimonialSection'
 import NewsSection from './components/NewsSection'
+import OnPage from '../common/OnPage'
+import BannerSlider from './components/BannerSlider'
+import VideoTestimonial from './components/VideoTestimonial'
 
 function HomePage() {
     return (
         <>
+        <OnPage MetaTitle="Realty Research - Best Real Estate Advisory in Noida"
+        Keywords="Realty Research, Noida real estate, property dealers Noida, buy property in Noida, best real estate company Noida"
+        MetaDesc="Discover top residential and commercial properties in Noida with Realty Research. Trusted experts, verified listings, and seamless real estate services."
+        />
         <HeroSection />
         <PropertyType />
         <UpcommingProject />
+        <BannerSlider />
         <ListingSection />
         <ImageSlider />
         <ProjectGallery />
         <TestimonialSection />
+        <VideoTestimonial />
         <NewsSection />
         </>
     )
